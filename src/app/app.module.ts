38f971abbafeac52703a3ffc0d06a3dc3acdf657
rauat <rauat@tuta.io>
2019-12-12 23:40:37 +0530

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './header/header.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { MatBadgeModule } from "@angular/material/badge";
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
