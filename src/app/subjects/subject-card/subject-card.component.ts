c0db284d344a891655f4238401da33ad080f72bb
rauat <rauat@tuta.io>
2019-12-17 03:20:57 +0530

    Enhanced subject card with arcs

--- src/app/subjects/subject-card/subject-card.component.ts
+++ src/app/subjects/subject-card/subject-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { textBox } from 'd3plus-text';
 
 @Component({
   selector: 'app-subject-card',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectCardComponent implements OnInit {
 
+
+  completionPercentage: number = 90;
+  author: string = 'Shishir Das';
+  title: string[] = ['Analog', 'Electronics'];
+  videosCount: number = 175;
+  subjectDuration: {
+    hours: 63,
+    minutes: 32,
+    seconds: 22
+  }
+
+  primaryColor: string = "#0000ff";
+  secondaryColor: string = "#1a3aa1";
+  
+
+  
+
   constructor() { }
+
+  setMaskValueBasedOnPercentage () {
+    return 450 - Math.round( 400 * this.completionPercentage / 100);
+  }
 
   ngOnInit() {
   }
